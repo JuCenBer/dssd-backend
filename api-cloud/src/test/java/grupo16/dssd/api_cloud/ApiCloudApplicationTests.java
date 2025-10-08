5cf7837364d07772af747837f32233ecb8a33eb0
Alvaro Aberastain Marzorati <a.aberastain.marzorati@gmail.com>
2025-10-08 20:29:05 -0300

    proyecto base api cloud

---
+++ api-cloud/src/test/java/grupo16/dssd/api_cloud/ApiCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package grupo16.dssd.api_cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
